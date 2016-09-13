2895323b49c563ea0f1360e5d57a6446a50a1523
CORO55 <CORO55>
2016-09-13 22:11:12 +0300

    cart

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function displayProds() {
                 product += "<span class='currency'>$</span>";
                 product += "<span class='price'>" + prod["price"] + "</span>";
                 product += "</div>";
-                product += "<button class='add glyphicon glyphicon-shopping-cart col-xs-4 col-sm-3 col-md-3 col-lg-3' data-id=" + prod['id'] + ">Add to cart</button>";
+                product += "<button class='add col-xs-4 col-sm-3 col-md-3 col-lg-3' data-id=" + prod['id'] + "><img src='res/shopping-cart.png' alt='cart'></img><span>Add to cart</span></button>";
                 product += "</div>";
                 $(".add-to-cart").append(product);
             }
@@ -46,8 +46,9 @@ function showFullCart() {
     $('.cart').append(fullCart);
 }
 
-function addProds() {
+function stageProds() {
     $('.add').on('click', function () {
+    var clicked = [];
         if ($('.cart-products').length === 0) {
             showFullCart();
         }
@@ -57,18 +58,30 @@ function addProds() {
             var index = prods[i];
             if (selectedProd == index.id) {
                 prodsToBeAdded.push(index);
+                clicked.push(index);
             }
         }
+        for (var i in clicked){
+            var prodInCart = clicked[i];
+            var cartProd = "<div class='prod-in-cart col-lg-12 col-md-12 col-sm-12 col-xs-12'>";
+            cartProd += "<div class='prod-name col-lg-6 col-md-6 col-sm-6 col-xs-6'><span class='name'>"+ prodInCart.name +"</span><img class='tooltip' src='/res/info.png'></img></div>";
+            cartProd += "<div class='quantity col-lg-2 col-md-2 col-sm-2 col-xs-2'><input type='text' value='1' class='form-control'></div>";
+            cartProd += "<div class='unit-price col-lg-4 col-md-4 col-sm-4 col-xs-4'><span class='currency'>$</span><span class='unit-val pull-left'>"+ prodInCart.price +"</span></div>";
+            cartProd += "<button class='remove'>X</button></div>";
+            $('.added-prods').append(cartProd);
+
+        }
+    clicked = [];
     });
 };
 
+
 $(document).ready(function () {
     getProducts();
-    $.cookie("products", prods);
     showEmptyCart();
 });
 
 $(document).ajaxComplete(function () {
     displayProds();
-    addProds();
+    stageProds();
 });
--- vars.js
+++ vars.js
@@ -3,8 +3,9 @@ var emptyCart = "<div class='no-prods'><span>No products in your shopping cart</
 var fullCart = "<div class='cart-products col-xs-12 col-sm-12 col-md-12 col-lg-12'><span>Products in your shopping cart</span></div>";
 fullCart += "<div class='table-header col-xs-12 col-sm-12 col-md-12 col-lg-12'>";
 fullCart += "<div class='product col-xs-6 col-sm-6 col-md-6 col-lg-6'><span>Product name</span></div>";
-fullCart += "<div class='qty col-xs-3 col-sm-3 col-md-3 col-lg-3'><span>Quantity</span></div>";
-fullCart += "<div class='value col-xs-3 col-sm-3 col-md-3 col-lg-3'><span>Value</span></div></div>";
+fullCart += "<div class='qty col-xs-2 col-sm-2 col-md-2 col-lg-2'><span class='pull-right'>Quantity</span></div>";
+fullCart += "<div class='value col-xs-4 col-sm-4 col-md-4 col-lg-4 pull-right'><span class='pull-right'>Value</span></div></div>";
 fullCart += "<div class='added-prods col-xs-12 col-sm-12 col-md-12 col-lg-12'></div>";
-fullCart += "<div class='total col-xs-12 col-sm-12 col-md-12 col-lg-12 pull-right'>";
-fullCart += "<span class='label'>Total: </span><span class='currency'>$</span><span class='amount-total'></span></div>";
+fullCart += "<div class='total col-xs-6 col-sm-6 col-md-3 col-lg-3 pull-right'>";
+fullCart += "<span class='label-amount'>Total: </span><span class='currency'>$</span><span class='amount-total'></span></div>";
+fullCart += "<button class='add continue col-xs-12 col-sm-12 col-md-12 col-lg-12'>Continue</button>";
