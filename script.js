459233230cf58d151f6cefec22ca2a598fe490ea
CORO55 <CORO55>
2016-09-14 22:57:42 +0300

    more stuff

--- script.js
+++ script.js
@@ -50,10 +50,16 @@ function calculateTotal() {
     var total = 0;
     $('.unit-val').each(function() {
         var price = Number($(this).text());
-        total = +total + +price;
-        console.log(total);
+        total += price;
     });
     $('.amount-total').html(parseFloat(total).toFixed(2));
+};
+
+function updateQtyAndPrice() {
+    $('.form-control').blur(function() {
+        $('.form-control').trigger('change');
+        console.log($(this).val());
+    });
 };
 
 function addProds() {
@@ -61,9 +67,8 @@ function addProds() {
         var clicked = [];
         if ($('.cart-products').length === 0) {
             showFullCart();
-        }
+        };
         var selectedProd = $(this).attr('data-id');
-        console.log(selectedProd + " clicked");
         for (var i in prods) {
             var index = prods[i];
             if (selectedProd == index.id) {
@@ -71,30 +76,41 @@ function addProds() {
                 clicked.push(index);
             }
             $('.' + selectedProd + '').detach();
-        }
+        };
+
+        function hideTooltipIfEmpty() {
+            for (var i in clicked) {
+                var prod = clicked[i];
+                if (prod.description == null) {
+                    $('#' + prod.id + '').hide();
+                }
+            }
+        };
+
         for (var i in clicked) {
             var prodInCart = clicked[i];
             var cartProd = "<div class='prod-in-cart col-lg-12 col-md-12 col-sm-12 col-xs-12 " + prodInCart.id + " data-id=" + prodInCart.id + "'>";
-            cartProd += "<div class='prod-name col-lg-6 col-md-6 col-sm-6 col-xs-6'><span class='name'>" + prodInCart.name + "<img src='res/info.png' class='tooltip-icon' alt='info'></img></span><div class='tooltip-text col-lg-2 col-md-2 col-sm-4 col-xs-4 " + prodInCart.id + "'>" + prodInCart.description + "</div></div>";
-            cartProd += "<div class='quantity col-lg-2 col-md-2 col-sm-2 col-xs-2'><input type='text' value='1' class='form-control'></div>";
-            cartProd += "<div class='unit-price col-lg-4 col-md-4 col-sm-4 col-xs-4'><span class='currency'>$</span><span class='unit-val'>" + prodInCart.price + "</span>";
+            cartProd += "<div class='prod-name col-lg-6 col-md-6 col-sm-6 col-xs-6'><span class='name'>" + prodInCart.name + "<img src='res/info.png' id=" + prodInCart.id + " class='tooltip-icon' alt='info'></img><div class='tooltip-text col-lg-2 col-md-2 col-sm-4 col-xs-4 " + prodInCart.id + "'>" + prodInCart.description + "</div></span></div>";
+            cartProd += "<div class='quantity col-lg-2 col-md-2 col-sm-2 col-xs-2'><input type='text' value='1' class='form-control' data-unit='" + prodInCart.id + "'></div>";
+            cartProd += "<div class='unit-price col-lg-4 col-md-4 col-sm-4 col-xs-4'><span class='currency'>$</span><span class='unit-val unit-" + prodInCart.id + "'>" + prodInCart.price + "</span>";
             cartProd += "<button class='remove glyphicon glyphicon-trash' data-remove-id='" + prodInCart.id + "'></button></div></div>";
             $('.added-prods').append(cartProd);
-
-        }
-
-        function showTooltip() {
-            $('.tooltip-icon').mouseenter(function() {
-                $('.tooltip-text.' + prodInCart.id + '').show();
-            });
-            $('.tooltip-icon').mouseleave(function() {
-                $('.tooltip-text.' + prodInCart.id + '').hide();
-            });
+            hideTooltipIfEmpty();
         };
 
+        function showTooltip(id) {
+            $('.tooltip-icon').mouseenter(function() {
+                id = $(this).attr('id');
+                $('.tooltip-text.' + id + '').show();
+            });
+            $('.tooltip-icon').mouseleave(function() {
+                $('.tooltip-text').hide();
+            });
+        };
         clicked = [];
         calculateTotal();
         showTooltip();
+        updateQtyAndPrice();
     });
 };
 
