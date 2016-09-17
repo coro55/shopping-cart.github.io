33b9baa880514550d2a15d48a5692a873635cf6a
CORO55 <CORO55>
2016-09-17 20:15:50 +0300

    data js fixes

--- js/data.js
+++ js/data.js
@@ -2,9 +2,9 @@ var emptyCart = "<div class='no-prods'><span>No products in your shopping cart</
 
 var fullCart = "<div class='full-cart'><div class='cart-products col-xs-12 col-sm-12 col-md-12 col-lg-12'><span>Products in your shopping cart</span></div>";
 fullCart += "<div class='table-header col-xs-12 col-sm-12 col-md-12 col-lg-12'>";
-fullCart += "<div class='product product-header col-xs-6 col-sm-6 col-md-6 col-lg-6'><span>Product name</span></div>";
-fullCart += "<div class='qty col-xs-2 col-sm-2 col-md-2 col-lg-2'><span class='pull-right'>Quantity</span></div>";
-fullCart += "<div class='value vlaue-label col-xs-3 col-sm-3 col-md-3 col-lg-3'><span class='pull-right'>Value</span></div></div>";
+fullCart += "<div class='product product-header col-xs-5 col-sm-5 col-md-6 col-lg-6'><span>Product name</span></div>";
+fullCart += "<div class='qty col-xs-2 col-sm-2 col-md-2 col-lg-2'><span>Quantity</span></div>";
+fullCart += "<div class='value value-label col-xs-4 col-sm-4 col-md-3 col-lg-3'><span class='pull-right'>Value</span></div></div>";
 fullCart += "<div class='added-prods product-section col-xs-12 col-sm-12 col-md-12 col-lg-12'></div>";
 fullCart += "<div class='total total-amount col-xs-6 col-sm-4 col-md-5 col-lg-4 pull-right'>";
 fullCart += "<span class='label-amount'>Total: </span><span class='currency'></span><span class='amount-total price'></span></div>";
